3f87cdd42fa092be12a07240b7f8ace8d5f55e0c
Devon Steenberg <devonsteenberg@hotmail.com>
2020-04-19 20:26:49 +1200

    Updated i3 config

---
+++ .i3/all-screens.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Turn on all displays
+if type "xrandr"; then
+  monitors=()
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    monitors+=( "$m" )
+  done
+  xrandr --output "${monitors[0]}" --auto
+  for m in ${!monitors[*]}; do
+    if [ "$m" -ne 0 ] 
+    then
+      i=$(($m - 1))
+      xrandr --output "${monitors[$m]}" --right-of "${monitors[$i]}"
+      # Launch polybar
+      MONITOR=HDMI-0 polybar -c ~/.i3/polybar-jaagr-space --reload top &
+    fi
+  done
+fi
---
+++ .i3/install-i3.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+sudo dnf install i3 i3status dmenu i3lock xbacklight feh conky polybar rofi -y
+
+#Install j4-dmenu-desktop
+sudo dnf install cmake g++ -y
+mkdir -p ~/development/i3
+cd ~/development/i3
+git clone https://github.com/enkore/j4-dmenu-desktop.git
+cd j4-dmenu-desktop
+cmake .
+make
+sudo make install
+
+# install the fonts
+#NotoSans
+sudo mkdir -p /usr/share/fonts/noto-sans
+sudo cp /home/devon/.i3/fonts/NotoSans/*.ttf /usr/share/fonts/noto-sans/
+
+#materialIcons
+sudo mkdir -p /usr/share/fonts/material-icons
+sudo cp /home/devon/.i3/fonts/MaterialIcons/*.ttf /usr/share/fonts/material-icons/
+
+#font awesome
+sudo mkdir -p /usr/share/fonts/font-awesome
+sudo cp /home/devon/.i3/fonts/FontAwesome/fontawesome-free-5.13.0-desktop/otfs/*.otf /usr/share/fonts/font-awesome/
+sudo cp /home/devon/.i3/fonts/FontAwesome/font-awesome-4.7.0/fonts/*.otf /usr/share/fonts/font-awesome/
+
+#unifont
+sudo mkdir -p /usr/share/fonts/unifont
+sudo cp /home/devon/.i3/fonts/unifont/gnu-unifont/*.ttf /usr/share/fonts/unifont/
+
+#refresh font cache
+fc-cache -v
+
+#Install xidlehook for autolocking
+sudo dnf install libxcb libXScrnSaver pulseaudio-libs -y
+sudo dnf install rust cargo -y
+cargo install xidlehook --bins
