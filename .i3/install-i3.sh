10502e8eb6660bcbf28369ad6c8e21cfeaac7c46
Devon Steenberg <devonsteenberg@hotmail.com>
2020-04-20 09:13:14 +1200

    Fixed i3 config

--- .i3/install-i3.sh
+++ .i3/install-i3.sh
@@ -16,19 +16,23 @@ sudo make install
 #NotoSans
 sudo mkdir -p /usr/share/fonts/noto-sans
 sudo cp /home/devon/.i3/fonts/NotoSans/*.ttf /usr/share/fonts/noto-sans/
+sudo find /usr/share/fonts/noto-sans -type f -exec chmod 644 -- {} +
 
 #materialIcons
 sudo mkdir -p /usr/share/fonts/material-icons
 sudo cp /home/devon/.i3/fonts/MaterialIcons/*.ttf /usr/share/fonts/material-icons/
+sudo find /usr/share/fonts/material-icons -type f -exec chmod 644 -- {} +
 
 #font awesome
 sudo mkdir -p /usr/share/fonts/font-awesome
 sudo cp /home/devon/.i3/fonts/FontAwesome/fontawesome-free-5.13.0-desktop/otfs/*.otf /usr/share/fonts/font-awesome/
 sudo cp /home/devon/.i3/fonts/FontAwesome/font-awesome-4.7.0/fonts/*.otf /usr/share/fonts/font-awesome/
+sudo find /usr/share/fonts/font-awesome -type f -exec chmod 644 -- {} +
 
 #unifont
 sudo mkdir -p /usr/share/fonts/unifont
 sudo cp /home/devon/.i3/fonts/unifont/gnu-unifont/*.ttf /usr/share/fonts/unifont/
+sudo find /usr/share/fonts/unifont -type f -exec chmod 644 -- {} +
 
 #refresh font cache
 fc-cache -v
