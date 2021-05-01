eab9b97cbcffda546b4d6e8bd21505e10f588d62
Devon Steenberg <devonsteenberg@hotmail.com>
2021-05-01 22:05:15 +1200

    Update doom cl setup

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -25,7 +25,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;(setq doom-theme 'doom-one)
+                                        ;(setq doom-theme 'doom-one)
 (setq doom-theme 'doom-nord)
 
 ;; If you use `org' and don't want your org files in the default location below,
@@ -54,23 +54,55 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(after! clojure-mode
-  (add-hook! 'clojure-mode-hook
-    (add-hook 'before-save-hook 'cider-format-buffer)))
+(add-hook! 'clojure-mode-hook
+  (add-hook 'before-save-hook #'cider-format-buffer))
 
-(map! :map clojure-mode-map
-      :localleader
-      :n "s f" 'paredit-forward-slurp-sexp
-      :n "s b" 'paredit-backward-slurp-sexp
-      :n "b f" 'paredit-forward-barf-sexp
-      :n "b b" 'paredit-backward-barf-sexp
+(map! (:localleader
+       :map clojure-mode-map
+       (:prefix ("s" . "slurp")
+        :desc "Slurp Forward" "f" #'paredit-forward-slurp-sexp
+        :desc "Slurp Backward" "b" #'paredit-backward-slurp-sexp)
+       (:prefix ("b" . "barf")
+        :desc "Barf Forward" "f" #'paredit-forward-barf-sexp
+        :desc "Barf Backward" "b" #'paredit-backward-barf-sexp)
+       (:prefix ("w" . "wrap")
+        :desc "Wrap Curly" "{" #'paredit-wrap-curly
+        :desc "Wrap Round" "(" #'paredit-wrap-round
+        :desc "Wrap Square" "[" #'paredit-wrap-square
+        :desc "Wrap Angled" "<" #'paredit-wrap-angled)
+       (:prefix ("f" . "form")
+        :desc "Format buffer" "f" #'cider-format-buffer
+        :desc "Raise Form" "r" #'paredit-raise-sexp
+        :desc "Splice Form" "s" #'paredit-splice-sexp
+        :desc "Transpose Form" "t" #'transpose-sexps)))
 
-      :n "w {" 'paredit-wrap-curly
-      :n "w (" 'paredit-wrap-round
-      :n "w [" 'paredit-wrap-square
-      :n "w <" 'paredit-wrap-angled
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max) nil))
 
-      :n "f r" 'paredit-raise-sexp
-      :n "f s" 'paredit-splice-sexp
+(add-hook! 'lisp-mode-hook
+  (add-hook 'before-save-hook #'indent-buffer))
 
-      :n "f t" 'transpose-sexps)
+(add-hook! 'lisp-mode-hook #'paredit-mode)
+
+(after! sly
+  (map! (:localleader
+         :map lisp-mode-map
+         (:prefix ("s" . "stickers + slurp")
+          :desc "Slurp Forward" "f" #'paredit-forward-slurp-sexp
+          :desc "Slurp Backward" "b" #'paredit-backward-slurp-sexp
+          :desc "Toggle breaking stickers" "B" #'sly-stickers-toggle-break-on-stickers
+          :desc "Fetch stickers"           "F" #'sly-stickers-fetch)
+         (:prefix ("b" . "barf")
+          :desc "Barf Forward" "f" #'paredit-forward-barf-sexp
+          :desc "Barf Backward" "b" #'paredit-backward-barf-sexp)
+         (:prefix ("w" . "wrap")
+          :desc "Wrap Curly" "{" #'paredit-wrap-curly
+          :desc "Wrap Round" "(" #'paredit-wrap-round
+          :desc "Wrap Square" "[" #'paredit-wrap-square
+          :desc "Wrap Angled" "<" #'paredit-wrap-angled)
+         (:prefix ("f" . "form")
+          :desc "Format buffer" "f" #'indent-buffer
+          :desc "Raise Form" "r" #'paredit-raise-sexp
+          :desc "Splice Form" "s" #'paredit-splice-sexp
+          :desc "Transpose Form" "t" #'transpose-sexps))))
