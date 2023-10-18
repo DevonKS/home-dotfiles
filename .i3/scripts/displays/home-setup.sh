dbe1df60f1ae55f1b79466c26bbb05464d3cdf3d
Devon Steenberg <devonsteenberg@hotmail.com>
2023-10-18 15:38:28 +1300

    Update i3 config

--- .i3/scripts/displays/home-setup.sh
+++ .i3/scripts/displays/home-setup.sh
@@ -14,16 +14,16 @@ if type "xrandr"; then
 fi
 
 if type "xrandr"; then
-  xrandr --output "eDP-1-1" --auto --output "HDMI-1-1" --auto --primary --right-of "eDP-1-1" --output "DP-2" --auto --right-of "HDMI-1-1"
+  xrandr --output "eDP-1-1" --auto --output "DP-2" --auto --right-of "eDP-1-1" --output "HDMI-1-1" --auto --primary --right-of "DP-2" 
   xrandr --dpi 96
 
   MONITOR="eDP-1-1" polybar -c ~/.i3/polybar/polybar-nord-minimal/config main &
-  MONITOR="HDMI-1-1" polybar -c ~/.i3/polybar/polybar-nord-minimal/config main &
   MONITOR="DP-2" polybar -c ~/.i3/polybar/polybar-nord-minimal/config main &
+  MONITOR="HDMI-1-1" polybar -c ~/.i3/polybar/polybar-nord-minimal/config main &
 
   i3-msg "workspace 1, move workspace to output eDP-1-1"
-  i3-msg "workspace 2, move workspace to output HDMI-1-1"
-  i3-msg "workspace 3, move workspace to output DP-2"
+  i3-msg "workspace 2, move workspace to output DP-2"
+  i3-msg "workspace 3, move workspace to output HDMI-1-1"
   i3-msg "workspace 2, focus"
 fi
 
