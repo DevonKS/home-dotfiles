7b72cc9b4a1d4a1d716af7a5f97c54914ebae47f
Devon Steenberg <devonsteenberg@hotmail.com>
2024-06-01 22:12:15 +1200

    Update home display config

--- .i3/scripts/displays/home-setup.sh
+++ .i3/scripts/displays/home-setup.sh
@@ -8,23 +8,21 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Turn of all displays
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    xrandr --output $m --off
-  done
-fi
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		xrandr --output $m --off
+	done
 
-if type "xrandr"; then
-  xrandr --output "eDP-1-1" --auto --output "DP-2" --auto --right-of "eDP-1-1" --output "HDMI-1-1" --auto --primary --right-of "DP-2" 
-  xrandr --dpi 96
+	xrandr --output "eDP" --auto --output "DP-1-2" --auto --primary --right-of "eDP" --output "HDMI-A-0" --auto --right-of "DP-1-2"
+	xrandr --dpi 96
 
-  MONITOR="eDP-1-1" polybar -c ~/.i3/polybar/polybar-nord-minimal/config main &
-  MONITOR="DP-2" polybar -c ~/.i3/polybar/polybar-nord-minimal/config main &
-  MONITOR="HDMI-1-1" polybar -c ~/.i3/polybar/polybar-nord-minimal/config main &
+	MONITOR="eDP" polybar -c ~/.i3/polybar/polybar-nord-minimal/config main &
+	MONITOR="DP-1-2" polybar -c ~/.i3/polybar/polybar-nord-minimal/config main &
+	MONITOR="HDMI-A-0" polybar -c ~/.i3/polybar/polybar-nord-minimal/config main &
 
-  i3-msg "workspace 1, move workspace to output eDP-1-1"
-  i3-msg "workspace 2, move workspace to output DP-2"
-  i3-msg "workspace 3, move workspace to output HDMI-1-1"
-  i3-msg "workspace 2, focus"
+	i3-msg "workspace 1, move workspace to output eDP"
+	i3-msg "workspace 2, move workspace to output DP-1-2"
+	i3-msg "workspace 3, move workspace to output HDMI-A-0"
+	i3-msg "workspace 2, focus"
 fi
 
 feh --bg-fill '/home/devon/.i3/wallpapers/NordWallpaperPack/daniel-leone-v7daTKlZzaw-unsplash.jpg'
